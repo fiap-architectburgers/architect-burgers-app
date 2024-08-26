7615d08384d06b3c8211c9b135d96e23759cdba7
Rodrigo Gomes <rodrigo.gomes@viasat.com>
2024-08-26 19:21:14 -0300

    terraform file cleanup

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-locals {
-  cluster_name = "fiap-architect-burgers-eks"
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.8.1"
@@ -98,78 +94,3 @@ resource "aws_db_instance" "burgers_db" {
   multi_az = true
 }
 
-
-## # EKS sera criado manualmente por enquanto.
-#    O modulo tenta fazer operações com Roles que não são permitidas no aws academy. Investigar depois...
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "20.8.5"
-#
-#   cluster_name    = local.cluster_name
-#   cluster_version = "1.29"
-#
-#   cluster_endpoint_public_access           = true
-#   enable_cluster_creator_admin_permissions = true
-#
-#   create_iam_role = false
-#   iam_role_name = "LabRole"
-#
-#   cluster_addons = {
-# #     aws-ebs-csi-driver = {
-# #       service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-# #     }
-#     coredns                = {}
-#     eks-pod-identity-agent = {}
-#     kube-proxy             = {}
-#     vpc-cni                = {}
-#   }
-#
-#   vpc_id     = module.vpc.vpc_id
-#   subnet_ids = module.vpc.private_subnets
-#
-#   eks_managed_node_group_defaults = {
-#     ami_type = "AL2_x86_64"
-#   }
-#
-#   eks_managed_node_groups = {
-#     one = {
-#       name = "node-group-1"
-#
-#       create_iam_role = false
-#       iam_role_name = "LabRole"
-#
-#       instance_types = ["t3.micro"]
-#
-#       min_size     = 3
-#       max_size     = 5
-#       desired_size = 3
-#     }
-# #
-# #     two = {
-# #       name = "node-group-2"
-# #
-# #       instance_types = ["t3.micro"]
-# #
-# #       min_size     = 2
-# #       max_size     = 3
-# #       desired_size = 2
-# #     }
-#   }
-# }
-#
-#
-# # https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/
-# data "aws_iam_policy" "ebs_csi_policy" {
-#   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-# }
-#
-# module "irsa-ebs-csi" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-#   version = "5.39.0"
-#
-#   create_role                   = true
-#   role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-#   provider_url                  = module.eks.oidc_provider
-#   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-#   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-# }
