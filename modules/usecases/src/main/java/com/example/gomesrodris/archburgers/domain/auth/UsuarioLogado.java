bc83cfb9d152d4d327405227c7877dde202da3f5
Rodrigo Gomes <rodrigo.gomes@viasat.com>
2024-08-31 17:03:14 -0300

    Estrutura basica para integracao com IdentityToken

---
+++ modules/usecases/src/main/java/com/example/gomesrodris/archburgers/domain/auth/UsuarioLogado.java
@@ -0,0 +1,18 @@
+package com.example.gomesrodris.archburgers.domain.auth;
+
+/**
+ * Representação de um usuário autenticado no sistema.
+ * É responsabilidade das camadas mais externas interagir com o mecanismo de autenticação e autorização, fornecendo
+ * instâncias válidas para a camada de use cases
+ */
+public interface UsuarioLogado {
+    boolean autenticado();
+
+    String getNome();
+    String getCpf();
+    String getEmail();
+
+    GrupoUsuario getGrupo();
+
+    String authError();
+}
