ae6279e0c61a7504df3299f4bd47e0ccabcf6624
Rodrigo Gomes <gomesrodrigo0481@gmail.com>
2024-09-19 19:29:57 -0300

    Add endpoint usuario/conectado

---
+++ modules/web-app/src/main/java/com/example/gomesrodris/archburgers/adapters/presenters/UsuarioLogadoPresenter.java
@@ -0,0 +1,17 @@
+package com.example.gomesrodris.archburgers.adapters.presenters;
+
+import com.example.gomesrodris.archburgers.domain.auth.UsuarioLogado;
+
+import java.util.Map;
+
+public class UsuarioLogadoPresenter {
+    public static Map<String, Object> toMap(UsuarioLogado usuarioLogado) {
+        return Map.of(
+                "nome", usuarioLogado.getNome(),
+                "email", usuarioLogado.getEmail(),
+                "cpf", usuarioLogado.getCpf(),
+                "grupo", usuarioLogado.getGrupo() != null ? usuarioLogado.getGrupo().name() : "",
+                "token", usuarioLogado.identityToken()
+        );
+    }
+}
